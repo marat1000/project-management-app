c9322a4192ca9261feeffd3b02192bc2d43c0b9f
Aleg3000 <88538863+Aleg3000@users.noreply.github.com>
2022-11-13 16:18:40 +0300

    init: set up eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <div>Task manager
-    </div>
-  );
+  return <div>Task manager</div>;
 }
 
 export default App;
