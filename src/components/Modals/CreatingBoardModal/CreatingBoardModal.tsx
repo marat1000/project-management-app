6c03894389838ed9f36525e6be057f228b99586c
Wsko-12 <vaskovladislav@gmail.com>
2022-11-21 13:58:08 +0300

    feat: add creating board modal

--- src/components/Modals/CreatingBoardModal/CreatingBoardModal.tsx
+++ src/components/Modals/CreatingBoardModal/CreatingBoardModal.tsx
@@ -1,18 +1,64 @@
+import { Button } from 'components/Button/Button';
 import { Modal } from 'components/Modals/Modal/Modal';
+import { useInput, useTextArea } from 'hooks/hooks';
 import React, { memo } from 'react';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
+import { createBoard, creatingBoardFlagsSelector } from 'store/slices/boardsSlice';
 import { selectCreatingBoardModalOpen, toggleCreatingBoardModal } from 'store/slices/modalsSlice';
 
 export const CreatingBoardModal = memo(() => {
   const isOpened = useAppSelector(selectCreatingBoardModalOpen);
+  const { error, isLoading } = useAppSelector(creatingBoardFlagsSelector);
   const dispatch = useAppDispatch();
   const toggle = (flag: boolean) => {
     dispatch(toggleCreatingBoardModal(flag));
   };
 
+  const title = useInput('');
+  const description = useTextArea('');
+
+  const submit = () => {
+    console.log('submit');
+    const boardTitle = title.value;
+    const boardDescription = description.value;
+    console.log('submit', boardTitle, boardDescription);
+
+    dispatch(
+      createBoard({
+        title: `${boardTitle}%${boardDescription}`,
+        users: [],
+      })
+    )
+      .unwrap()
+      .then(() =>
+        // TODO create middleware
+        toggle(false)
+      );
+  };
+
+  if (isLoading) {
+    return (
+      <Modal isOpened={isOpened} toggle={toggle} title="Create board">
+        Loading
+      </Modal>
+    );
+  }
+
+  if (error) {
+    return (
+      <Modal isOpened={isOpened} toggle={toggle} title="Create board">
+        {error}
+      </Modal>
+    );
+  }
+
   return (
-    <Modal isOpened={isOpened} toggle={toggle}>
-      CreatingBoardModal
+    <Modal isOpened={isOpened} toggle={toggle} title="Create board">
+      <input placeholder="Board name" {...title}></input>
+      <textarea placeholder="Description" {...description}></textarea>
+      <Button color="add" onClick={submit}>
+        Create
+      </Button>
     </Modal>
   );
 });
--- src/components/Modals/Modal/Modal.tsx
+++ src/components/Modals/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { memo, ReactNode } from 'react';
+import React, { memo, ReactNode, SyntheticEvent } from 'react';
 import { createPortal } from 'react-dom';
 import { SmoothCorners } from 'react-smooth-corners';
 
@@ -6,14 +6,27 @@ interface IModalProps {
   isOpened: boolean;
   toggle: (flag: boolean) => void;
   children: ReactNode;
+  title: string;
 }
 
-export const Modal = memo<IModalProps>(({ children, isOpened, toggle }) => {
+export const Modal = memo<IModalProps>(({ title, children, isOpened, toggle }) => {
   if (!isOpened) return null;
 
   const element = (
     <div className="modal__overlay" onClick={() => toggle(false)}>
-      <SmoothCorners corners="22, 16" borderRadius="25px" as="div" className="modal__container">
+      <SmoothCorners
+        corners="22, 16"
+        borderRadius="25px"
+        as="div"
+        className="modal__container"
+        onClick={(e: SyntheticEvent) => e.stopPropagation()}
+      >
+        <div className="modal__header">
+          <h2>{title}</h2>
+          <button onClick={() => toggle(false)} className="modal__close">
+            ✖
+          </button>
+        </div>
         {children}
       </SmoothCorners>
     </div>
