4be5e2a4a3c0209653fa9363bd03d799d6b6e8d2
Aleg3000 <88538863+Aleg3000@users.noreply.github.com>
2022-11-22 15:35:48 +0530

    fix: go to main page buttons

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,16 +1,30 @@
 import { Button } from 'components/Button/Button';
 import { StartPagesLayout } from 'components/StartPagesLayout/StartPagesLayout';
 import React, { memo } from 'react';
+import { useAppSelector } from 'store/hooks';
+import { authSelector } from 'store/slices/authSlice';
 import { ERoutes } from 'ts/enums';
 export const Welcome = memo(() => {
+  const isAuth = useAppSelector(authSelector);
   return (
     <StartPagesLayout>
       <div className="welcome__image">
         <div className="welcome__buttons-container">
-          <Button to={ERoutes.singIn}>Sign in</Button>
-          <Button to={ERoutes.singUp} color="add">
-            Sign up
-          </Button>
+          {isAuth ? (
+            <>
+              <Button to={ERoutes.main}>Go to main page</Button>
+              <Button to={ERoutes.main} color="add">
+                Go to main page
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button to={ERoutes.singIn}>Sign in</Button>
+              <Button to={ERoutes.singUp} color="add">
+                Sign up
+              </Button>
+            </>
+          )}
         </div>
       </div>
     </StartPagesLayout>
