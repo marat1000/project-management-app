ff9bda048d46d7afe6963f68e375b4de2b564473
Wsko-12 <vaskovladislav@gmail.com>
2022-12-03 19:13:23 +0300

    fix: remove useless lib

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,8 +1,10 @@
+import { langConfig } from 'language/langConfig';
 import React, { memo } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useAppSelector } from 'store/hooks';
+import { selectLanguage } from 'store/slices/settings/settingsSelectors';
 
 export const Page404 = memo(() => {
-  const { t } = useTranslation();
+  const lang = useAppSelector(selectLanguage);
 
-  return <p className="error-404">{t(`message404`)}</p>;
+  return <p className="error-404">{langConfig.message404[lang]}</p>;
 });
