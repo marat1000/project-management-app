ad72c5e322ba950ea969f2bd13dd185747315ef7
unknown <marat76593@gmail.com>
2022-12-05 18:15:49 +0300

    fix: merge commit

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -17,38 +17,38 @@ export const Welcome = memo(() => {
       <div className={`welcome`}>
         <StartPagesLayout>
           <SmoothCorners
-              corners="22, 16"
-              borderRadius="25px"
-              as="div"
-              className="welcome__inner-wrapper"
+            corners="22, 16"
+            borderRadius="25px"
+            as="div"
+            className="welcome__inner-wrapper"
           >
-        <div className="welcome__image">
-          <div className="welcome__buttons-container">
-            {isAuth ? (
-              <>
-                <ErrorBoundary>
-                  <Button to={ERoutes.main}>{langConfig.goToMainPage[lang]}</Button>
-                </ErrorBoundary>
-                <ErrorBoundary>
-                  <Button to={ERoutes.main} color="add">
-                    {langConfig.goToMainPage[lang]}
-                  </Button>
-                </ErrorBoundary>
-              </>
-            ) : (
-              <>
-                <ErrorBoundary>
-                  <Button to={ERoutes.singIn}>{langConfig.signIn[lang]}</Button>
-                </ErrorBoundary>
-                <ErrorBoundary>
-                  <Button to={ERoutes.singUp} color="add">
-                    {langConfig.signUp[lang]}
-                  </Button>
-                </ErrorBoundary>
-              </>
-            )}
-          </div>
-        </div>
+            <div className="welcome__image">
+              <div className="welcome__buttons-container">
+                {isAuth ? (
+                  <>
+                    <ErrorBoundary>
+                      <Button to={ERoutes.main}>{langConfig.goToMainPage[lang]}</Button>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                      <Button to={ERoutes.main} color="add">
+                        {langConfig.goToMainPage[lang]}
+                      </Button>
+                    </ErrorBoundary>
+                  </>
+                ) : (
+                  <>
+                    <ErrorBoundary>
+                      <Button to={ERoutes.singIn}>{langConfig.signIn[lang]}</Button>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                      <Button to={ERoutes.singUp} color="add">
+                        {langConfig.signUp[lang]}
+                      </Button>
+                    </ErrorBoundary>
+                  </>
+                )}
+              </div>
+            </div>
           </SmoothCorners>
         </StartPagesLayout>
         <section className={`welcome__school-info`}>
